0a927ec7f849698cca612f4f58c0ade00f652d87
Amara Shein <ashein00@stuy.edu>
2019-02-08 11:33:51 -0500

    made constructor and working on addQueen

--- QueenBoard.java
+++ QueenBoard.java
@@ -4,11 +4,55 @@ public class QueenBoard{
   private int[][]board;
 
   // constructor
-  public QueenBoard(int size){}
+  public QueenBoard(int size){
+    board = new int[size][size];
+    for (int x = 0; x < size; x++){
+      for (int y = 0; y < size; y++){
+        board[x][y] = 0;
+      }
+    }
+  }
 
   // private methods (suggested)
-  private boolean addQueen(int r, int c){}
-  private boolean removeQueen(int r, int c){}
+  private boolean addQueen(int r, int c){
+    int big = board.size();
+    // if it is an empty, nonthreatening space, place a queen there and return true
+    if (board[r][c] == 0){
+      // making the space have a queen
+      board[r][c] = -1;
+
+      // changing values across the board
+      for (int across = r; across < big && across > 0; across++){
+        board[across][c] = board[across][c] + 1;
+      }
+      for (int across = r; across < big && across > 0; across--){
+        board[across][c] = board[across][c] + 1;
+      }
+
+      // changing values down the board
+      for (int down = c; down < big && big > 0; big++){
+        board[r][down] = board[r][down] + 1;
+      }
+      for (int down = c; down < big && big > 0; big--){
+        board[r][down] = board[r][down] + 1;
+      }
+
+      // changing values diagonal the board
+
+      return true;
+    }
+    // else, don't add anything and return false
+    return false;
+  }
+  private boolean removeQueen(int r, int c){
+    // if there is a queen in the specified location, remove the queen there and return true
+    if (board[r][c] == -1){
+      board[r][c] = 0;
+      return true;
+    }
+    // else, don't remove anything and return false
+    return false;
+  }
 
   // public methods
   /**
