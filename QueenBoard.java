f446560f3eb178c952d69ca68f119fff888f23c0
sheinamara <ashein00@stuy.edu>
2019-02-25 22:51:48 -0500

    added code from Mr. K

--- QueenBoard.java
+++ QueenBoard.java
@@ -318,4 +318,24 @@ public class QueenBoard{
     }
     return numSolutions;
   }
+
+  // FROM MR. K
+  //testcase must be a valid index of your input/output array
+public static void runTest(int i){
+  QueenBoard b;
+  int[]tests =   {1,2,3,4,5,8};
+  int[]answers = {1,0,0,2,10,92};
+  if(i >= 0 && i < tests.length ){
+    int size = tests[i];
+    int correct = answers[i];
+    b = new QueenBoard(size);
+    int ans  = b.countSolutions();
+
+    if(correct==ans){
+      System.out.println("PASS board size: "+tests[i]+" "+ans);
+    }else{
+      System.out.println("FAIL board size: "+tests[i]+" "+ans+" vs "+correct);
+    }
+  }
+}
 }
